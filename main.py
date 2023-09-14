07ee36d949182323aa20f2ecba5010a9e214c53e
Carlos Correa <carlosdcorrea3@gmail.com>
2023-09-14 17:39:41 -0500

    The code has being organized thanks to some example code in this link https://github.dev/bcbio/bcbio-nextgen-vm/blob/cd703e9d0dd5c6e25ddefc39efd9fd87ec815615/scripts/bcbio_vm.py#L145#L145

--- main.py
+++ main.py
@@ -3,38 +3,81 @@ import sqlite3
 from datetime import date
 
 
-parser = argparse.ArgumentParser(
-                    prog="Orme",
-                    description="This program allows the user to register all its expenses",
-                    epilog="TechSsus - Carlos Correa"   
-                 )
+def validate_date(str_date):
+   try:
+      date.fromisoformat(str_date)
+   except ValueError as e:
+      print(e)
 
-""" Global arguments
-- expense 
-   - add
-   - edit
-   - delete
-"""
-subparsers = parser.add_subparsers(required=True)
-""" add Sub Argument
-Add something
-"""
-parser_add = subparsers.add_parser('add', help='adds a new expense with all its attributes')
-parser_add.add_argument('type', help='the type of register to add', choices=['expense', 'income'])
-#parser_expense.add_argument('date', type=str, help='The date of this particular expense (not the register)')
-#parser_expense.add_argument('desc', type=str, help='The description of this particular expense')
+def list_expenses(args):
+   print('running list expenses method', args.type)
 
-args = vars(parser.parse_args())
-print('args::', args)
+def run_list_expenses(subparser):
+   parser_list = subparsers.add_parser('list', help='list expenses base on specific filters')
+   parser_list.add_argument('type', help='the type of register to list', choices=['expense', 'income'])
+   parser_list.add_argument('-all', help='list all expenses', action='store_true')
+   parser_list.set_defaults(func=list_expenses)
 
-con = sqlite3.connect('orme.db')
-cur = con.cursor()
+def create_expense(args):
+   validate_date(args.date)
+   is_divided = 1 if args.div else 0
+    
+   con = sqlite3.connect('orme.db')
+   cur = con.cursor()
+   
+   create_expenses_table_query = """
+   CREATE TABLE if not exists expenses(
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      value INTEGER NOT NULL, 
+      description TEXT, 
+      is_divided INTEGER, 
+      date DATE
+      )
+   """
+   
+   insert_into_expenses_query = f"""
+   INSERT INTO expenses(
+      value, 
+      description, 
+      is_divided, 
+      date) VALUES(
+         {args.val}, 
+         '{args.desc}', 
+         {is_divided}, 
+         '{args.date}'
+         )
+   """
+   
+   cur.execute(create_expenses_table_query)
+   cur.execute(insert_into_expenses_query) 
 
-cur.execute('CREATE TABLE if not exists expenses(value)')
+   con.commit()
 
-cur.execute(f"""INSERT INTO expenses VALUES({args['val']})""")
+   cur.close()
+   con.close()
+   
+   print('The expense was registered successfully...')
+    
 
-con.commit()
+def run_create_expense(subparser):
+   parser_add = subparsers.add_parser('add', help='adds a new expense with all its attributes')
+   parser_add.add_argument('type', help='the type of register to add', choices=['expense', 'income'])
+   parser_add.add_argument('-val', type=int, help='The  of this particular expense (not the register)', required=True)
+   parser_add.add_argument('-desc', type=str, help='The description of this particular expense', required=True)
+   parser_add.add_argument('-date', type=str, help='Date of this particular expense (not the register date but the execute one) - default: current day', default=date.today().isoformat())
+   parser_add.add_argument('-div', help='Whether this expense should be divided by 2 for calculation purposes', action='store_true')
+   parser.set_defaults(func=create_expense)
 
-cur.close()
-con.close()
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(
+                     prog="Orme",
+                     description="This program allows the user to register all its expenses",
+                     epilog="TechSsus - Carlos Correa"   
+                    )
+
+   subparsers = parser.add_subparsers(title='[sub-commands]')
+   run_create_expense(subparsers)
+   run_list_expenses(subparsers)
+   
+   args = parser.parse_args()
+   args.func(args)
