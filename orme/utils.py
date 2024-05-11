8477fae7de287b553e036a66d32abeebe2ffd0ff
Carlos Correa <carlosdcorrea3@gmail.com>
2024-05-11 15:06:07 -0500

    Get Operator
    Types have been extended so it is more clear what methods take and return
    
    Get Operator method added to get the operator of every where statement

--- orme/utils.py
+++ orme/utils.py
@@ -1,12 +1,27 @@
 import types
 from argparse import Namespace
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 
-def get_present_arguments(args: Namespace) -> List[Tuple]:
+def get_present_arguments(args: Namespace) -> List[Tuple[str, Union[str, int]]]:
     present_arguments = [
         (argument, value) for argument, value in vars(args).items()
         if value is not None and not isinstance(value, types.FunctionType)
     ]
 
     return present_arguments
+
+
+def get_operator(args: List[Tuple[str, Union[str, int]]]) -> str | None:
+    print(args)
+    match args:
+        case [(arg, _)] if arg.startswith('bt'):
+            return 'bt'
+        case [(arg, _)] if arg.startswith('gt'):
+            return 'gt'
+        case [(arg, _)] if arg.startswith('lt'):
+            return 'lt'
+        case [(arg, _)] if arg.startswith('eq'):
+            return '='
+        case _:
+            return None
