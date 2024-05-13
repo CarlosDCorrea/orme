ade97f984e79bc82568c05cfe7ba805f103622fd
Carlos Correa <carlosdcorrea3@gmail.com>
2024-05-13 15:13:27 -0500

    Changes in the run modules for both debt and expenses
    - Now this module are abstraction to the real executions so it is easy to track where the changes should be done

--- orme/expenses/run_expenses.py
+++ orme/expenses/run_expenses.py
@@ -1,20 +1,49 @@
-from ..db.connection import create_connection_and_execute_query
+from argparse import Namespace
+from typing import Tuple, List
+
+from orme.utils import get_present_arguments
+
+from orme.db.connection import create_connection_and_execute_query
+from orme.settings import (QUERY_CREATE,
+                           QUERY_LIST,
+                           QUERY_UPDATE,
+                           QUERY_DELETE)
+from orme.db.queries.queries_expenses import generate_create_query, generate_list_query
 
 
 TABLE_NAME = 'expenses'
 
 
-def create_expense(args):
-    create_connection_and_execute_query(TABLE_NAME, 'create', args)
+def define_query(query_type: int, args: Namespace) -> str:
+    present_arguments: List[Tuple] = get_present_arguments(args)
+
+    queries: List[str] = []
+
+    if query_type == QUERY_CREATE:
+        print(present_arguments)
+        queries = generate_create_query(args)
+    if query_type == QUERY_LIST:
+        queries = generate_list_query(present_arguments)
+        pass
+    if query_type == QUERY_UPDATE:
+        pass
+    if query_type == QUERY_DELETE:
+        pass
+
+    return queries
 
 
-def list_expenses(args):
-    create_connection_and_execute_query(TABLE_NAME, 'list', args)
+def create_expense(args: Namespace):
+    create_connection_and_execute_query('create', define_query(QUERY_CREATE, args), TABLE_NAME)
 
 
-def generate_graph(args):
+def list_expenses(args: Namespace):
+    create_connection_and_execute_query('list', define_query(QUERY_LIST, args), TABLE_NAME)
+
+
+def update_expense(args: Namespace) -> None:
     pass
 
 
-def generate_stats(args):
+def delete_expense(args: Namespace) -> None:
     pass
