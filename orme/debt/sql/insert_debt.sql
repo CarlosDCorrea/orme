6b728cd2d595a0d040bcce643ef5c5a691d415b6
Carlos Correa <carlosdcorrea3@gmail.com>
2024-10-22 09:21:58 -0500

    Experimentation 001
    I was just trying to explore new ways of making and executing queries by create .sql files,
    I ended up not seeing the point in doing it for in app queries since python string formatting is good enough,
    It could be used for migrations instead, but overall I think It would make the code more complex for in app queries

---
+++ orme/debt/sql/insert_debt.sql
@@ -0,0 +1,10 @@
+INSERT INTO debts(
+    value,
+    deptor,
+    lender,
+    description,
+    interest_rate,
+    months,
+    date,
+    created,
+    updated) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);
