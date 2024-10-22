081723e83149d2443eb11ebebbfc6c38a6cc5842
Carlos Correa <carlosdcorrea3@gmail.com>
2024-10-22 09:26:53 -0500

    Refers to the previous commit

---
+++ orme/db/queries/queries_debts.py
@@ -0,0 +1,34 @@
+from argparse import Namespace
+from datetime import date
+from typing import Tuple
+
+
+TABLE_NAME = 'debts'
+
+
+def generate_create_query(args: Namespace) -> Tuple[str, str]:
+    today: str = date.today().isoformat()
+
+    insert_into_debts_query = f"""
+    INSERT INTO {TABLE_NAME}(
+        value,
+        deptor,
+        lender,
+        description,
+        interest_rate,
+        months,
+        date,
+        created,
+        updated) VALUES(
+            {args.value},
+            '{args.deptor}',
+            '{args.lender}',
+            '{args.description}',
+            {args.interest_rate},
+            {args.months},
+            '{args.date}',
+            '{today}',
+            '{today}'
+            )"""
+
+    return (create_debts_table_query, insert_into_debts_query)
