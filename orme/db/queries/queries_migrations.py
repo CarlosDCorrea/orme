7dbc18a538dc823e2b9d8d1491a1ad009a3dbc8f
Carlos Correa <carlosdcorrea3@gmail.com>
2024-11-21 08:08:15 -0500

    C: insert into query modified so it is built from the input dinamically, this could be a common query for insert into since it is dinamic

--- orme/db/queries/queries_migrations.py
+++ orme/db/queries/queries_migrations.py
@@ -1,11 +1,11 @@
-from datetime import date
-from typing import Tuple
+from typing import Tuple, Dict
 
 
 TABLE_NAME = 'migrations'
 
 
-def create() -> Tuple[str]:
+# All creations should be in migrations files
+def create_table() -> Tuple[str]:
     create_query = f"""
     CREATE TABLE if not exists {TABLE_NAME}(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,28 +19,24 @@ def create() -> Tuple[str]:
     return (create_query,)
 
 
-def insert_into(*values: Tuple[str]) -> Tuple[str]:
-    model: str = values[0]
-    name: str = values[1]
-    today = date.today().isoformat()
-
+def insert_into(data: Dict[str, Tuple[str | int]]) -> Tuple[str]:
+    character = ', \n'
     insert_into: str = f"""
     INSERT INTO {TABLE_NAME}(
-        name,
-        state,
-        created,
-        updated) VALUES(
-            '{model}',
-            '{name}',
-            '{today}',
+        {character.join(data)}) VALUES(
+        {character.join([f"'{value}'"
+                         if isinstance(value, str)
+                         else value
+                         for value in data.values()])}
             );"""
 
-    return (insert_into,)
+    return ('', insert_into)
 
 
-def list_() -> Tuple[str]:
+def list_(model: str) -> Tuple[str]:
     list_query: str = f"""
-    SELECT * FROM {TABLE_NAME};
+    SELECT * FROM {TABLE_NAME}
+    WHERE model = '{model}';
     """
 
     return (list_query,)
