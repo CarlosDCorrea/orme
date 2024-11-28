d78aad042d349f8c8f5ae65ecfb399325d6d7cbf
Carlos Correa <carlosdcorrea3@gmail.com>
2024-11-28 12:43:49 -0500

    Changes, Fixes and Additions
    C: Now exceptions for every command or option are handle when their respective functions are called
    C: Now the user option in the add expenses command is not required
    F: Fix in the insert into generation query in common queries, int data types can be joined with string.join, every int is treat as a string in order to solve it, the values are still stored as ints tho
    A: new method added to utils, get_dict_present_arguments allows getting dicts_based_args for the creation/insertion of new records

--- orme/expense/run_expenses.py
+++ orme/expense/run_expenses.py
@@ -1,12 +1,13 @@
+from datetime import date
 from argparse import Namespace
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
-from orme.utils import get_present_arguments
+from orme.utils import get_present_arguments, get_dict_present_arguments
 
 from orme.db.connection import create_connection_and_execute_query
 
-from orme.db.queries.queries_expenses import generate_create_query
-from orme.db.queries.common_queries import (generate_list_query,
+from orme.db.queries.common_queries import (generate_insert_into_query,
+                                            generate_list_query,
                                             generate_update_query,
                                             generate_delete_query,
                                             generate_total_query)
@@ -16,7 +17,18 @@ TABLE_NAME = 'expenses'
 
 
 def create_expense(args: Namespace) -> None:
-    queries = generate_create_query(args)
+    present_arguments: Dict[str, str | int | List[str | int]] = get_dict_present_arguments(args)
+    today: str = date.today().isoformat()
+
+    if 'div' in args:
+        is_divided: int = 1 if present_arguments['div'] else 0
+        del present_arguments['div']
+        present_arguments['is_divided'] = is_divided
+
+    present_arguments['created'] = today
+    present_arguments['updated'] = today
+
+    queries: List[str] = generate_insert_into_query(present_arguments, TABLE_NAME)
     create_connection_and_execute_query(
         'create', queries, TABLE_NAME)
 
@@ -30,7 +42,10 @@ def list_expenses(args: Namespace) -> None:
 
 def update_expense(args: Namespace) -> None:
     present_arguments: List[Tuple] = get_present_arguments(args)
+
+    print('inside update expense')
     if len(present_arguments) == 1:
+        print('throwing exception')
         raise ValueError('This command requires the fields to be updated')
 
     queries = generate_update_query(present_arguments, TABLE_NAME)
